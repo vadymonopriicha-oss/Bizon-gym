471fa38ef9e2887600f879b17f2a6bc076004132
vadymonopriicha-oss <vadymonopriicha@gmail.com>
2026-09-16 13:37:08 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,20 +1,3 @@
-CREATE TABLE IF NOT EXISTS users(
- id BIGSERIAL PRIMARY KEY,
- email TEXT UNIQUE NOT NULL,
- password_hash TEXT NOT NULL,
- is_admin BOOLEAN NOT NULL DEFAULT FALSE,
- created_at TIMESTAMPTZ DEFAULT now(),
- ext_order_id TEXT UNIQUE,
- payu_order_id TEXT,
- program_id BIGINT REFERENCES programs(id)
-);
-CREATE TABLE IF NOT EXISTS entitlements(
- user_id BIGINT REFERENCES users(id),
- program_id BIGINT REFERENCES programs(id),
- order_id BIGINT REFERENCES orders(id),
- granted_at TIMESTAMPTZ DEFAULT now(),
- PRIMARY KEY(user_id,program_id)
-);
 CREATE TABLE IF NOT EXISTS programs(
  id BIGSERIAL PRIMARY KEY,
  title TEXT NOT NULL,
@@ -25,16 +8,37 @@ CREATE TABLE IF NOT EXISTS programs(
  content_url TEXT,
  created_at TIMESTAMPTZ DEFAULT now()
 );
+
+CREATE TABLE IF NOT EXISTS users(
+ id BIGSERIAL PRIMARY KEY,
+ email TEXT UNIQUE NOT NULL,
+ password_hash TEXT NOT NULL,
+ is_admin BOOLEAN NOT NULL DEFAULT FALSE,
+ created_at TIMESTAMPTZ DEFAULT now()
+);
+
 CREATE TABLE IF NOT EXISTS orders(
  id BIGSERIAL PRIMARY KEY,
  user_id BIGINT REFERENCES users(id),
- status TEXT NOT NULL DEFAULT 'pending',
+ status TEXT NOT NULL DEFAULT 'PENDING',
  total_pln NUMERIC(10,2) NOT NULL,
+ ext_order_id TEXT UNIQUE,
+ payu_order_id TEXT,
+ program_id BIGINT REFERENCES programs(id),
  created_at TIMESTAMPTZ DEFAULT now()
 );
+
 CREATE TABLE IF NOT EXISTS order_items(
  order_id BIGINT REFERENCES orders(id),
  program_id BIGINT REFERENCES programs(id),
  price_pln NUMERIC(10,2) NOT NULL,
  PRIMARY KEY(order_id,program_id)
+);
+
+CREATE TABLE IF NOT EXISTS entitlements(
+ user_id BIGINT REFERENCES users(id),
+ program_id BIGINT REFERENCES programs(id),
+ order_id BIGINT REFERENCES orders(id),
+ granted_at TIMESTAMPTZ DEFAULT now(),
+ PRIMARY KEY(user_id,program_id)
 );
